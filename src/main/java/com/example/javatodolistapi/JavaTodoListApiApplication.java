61edbb37f5d65d21856682bd5e42260a27aa2be6
Heelkindy <vanhieu2241998@gmail.com>
2025-10-27 21:19:13 +0700

    Initial commit

---
+++ src/main/java/com/example/javatodolistapi/JavaTodoListApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.javatodolistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTodoListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTodoListApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javatodolistapi/JavaTodoListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javatodolistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTodoListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
