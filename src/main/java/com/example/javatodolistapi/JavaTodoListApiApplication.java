fc24244a21020fe047427cff62917dce4e6a0982
Heelkindy <vanhieu2241998@gmail.com>
2025-10-27 21:19:34 +0700

    init resource

--- src/main/java/com/example/javatodolistapi/JavaTodoListApiApplication.java
+++ src/main/java/com/example/javatodolistapi/JavaTodoListApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaTodoListApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaTodoListApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTodoListApiApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/javatodolistapi/JavaTodoListApiApplicationTests.java
+++ src/test/java/com/example/javatodolistapi/JavaTodoListApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JavaTodoListApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
